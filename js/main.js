92813550dda0a834bbf57f6cdb3234f6e9ed92d3
Longer <sgzlonger@gmail.com>
2018-03-04 17:43:01 +0100

    added responsive navigation

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@
 
 const hamburgerIcon = document.querySelector('.hamburger-icon');
 const hamburgerElement = document.querySelector('.hamburger-icon__element');
+const nav = document.querySelector('.navigation__list');
 
 hamburgerIcon.addEventListener('click', function() {
     hamburgerElement.classList.toggle('clicked');
+    nav.classList.toggle('navigation__list--slide')
 });

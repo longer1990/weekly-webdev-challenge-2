d2707ef9e347cd455a5b4ca07b359ff0c2b1a2a0
Longer <sgzlonger@gmail.com>
2018-03-28 21:19:24 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -6,14 +6,31 @@
 */
 
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function() {
 
-  const hamburgerBtn = document.querySelector('.hamburger-btn');
-  const hamburgerElement = document.querySelector('.hamburger-btn__element');
-  const nav = document.querySelector('.navigation__list');
+    const hamburgerBtn = document.querySelector('.hamburger-btn');
+    const hamburgerElement = document.querySelector('.hamburger-btn__element');
+    const nav = document.querySelector('.navigation__list');
+    const navLink = document.querySelectorAll('.navigation__link');
 
-  hamburgerBtn.addEventListener('click', function() {
-      hamburgerElement.classList.toggle('clicked');
-      nav.classList.toggle('navigation__list--slide')
-});
+    hamburgerBtn.addEventListener('click', function() {
+        hamburgerElement.classList.toggle('clicked');
+        nav.classList.toggle('navigation__list--slide')
+    });
+
+/*
+    for (var i = 0; i < navLink.length; i++) {
+        navLink[i].addEventListener('click', function() {
+            nav.classList.remove('navigation__list--slide');
+            hamburgerElement.classList.toggle('clicked');
+        });
+    }; */
+
+    navLink.forEach(function(el){
+      el.addEventListener('click', function(){
+        nav.classList.remove('navigation__list--slide');
+        hamburgerElement.classList.toggle('clicked');
+      });
+    })
+
 })
